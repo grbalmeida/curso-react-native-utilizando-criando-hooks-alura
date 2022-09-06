aff860efa959c12ad7e803320fdfac17667b13c4
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-06 10:43:00 -0300

    Estilizar a lista #2

--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,13 +1,59 @@
 import React from 'react';
 
-import { View, Image, Text } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 
 export default function Produtor({ nome, imagem, distancia, estrelas }) {
-    return <View>
-        <Image source={imagem} accessibilityLabel={nome} />
-        <View>
-            <Text>{ nome }</Text>
-            <Text>{ distancia }</Text>
+    return <View style={estilos.cartao}>
+        <Image source={imagem} style={estilos.imagem} accessibilityLabel={nome} />
+        <View style={estilos.informacoes}>
+            <Text style={estilos.nome}>{ nome }</Text>
+            <Text style={estilos.distancia}>{ distancia }</Text>
         </View>
     </View>
 }
+
+const estilos = StyleSheet.create({
+    cartao: {
+        backgroundColor: '#F6F6F6',
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 6,
+        flexDirection: 'row',
+
+        // Android
+        elevation: 4,
+
+        // iOS
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+    },
+    imagem: {
+        width: 48,
+        height: 48,
+        borderRadius: 6,
+        marginVertical: 16,
+        marginLeft: 16,
+    },
+    informacoes: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginLeft: 8,
+        marginVertical: 16,
+        marginRight: 16,
+    },
+    nome: {
+        fontSize: 14,
+        lineHeight: 22,
+        fontWeight: 'bold',
+    },
+    distancia: {
+        fontSize: 12,
+        lineHeight: 19,
+    }
+});
